850082045175f959fd01f3804fcf16e108116b6d
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-01-24 09:10:02 -0700

    Added skeleton classes to deck, round, also skel methods.

---
+++ lib/deck.rb
@@ -0,0 +1,8 @@
+class Deck
+
+  def initialize
+      #array of card objects
+  end
+    #needs to return cards based on a category
+
+end
---
+++ lib/round.rb
@@ -0,0 +1,8 @@
+class Round
+
+  def initialize
+    #array of card objects
+  end
+  #needs to return cards based on a category
+
+end
