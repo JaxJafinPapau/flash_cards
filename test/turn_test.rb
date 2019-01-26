48320d08da5e918e4a5362332ea42e6992033ea9
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-01-25 21:18:27 -0700

    Added setup method to turn_test and changed assoc ivar

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -6,37 +6,36 @@ require 'pry'
 
 class TurnTest < Minitest::Test
 
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  end
+
   def test_turn_exists
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card_1)
+    turn = Turn.new("Juneau", @card_1)
 
     assert_instance_of Turn, turn
   end
 
   def test_turn_has_a_card
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card_1)
+    turn = Turn.new("Juneau", @card_1)
 
-    assert_equal card_1, turn.card
+    assert_equal @card_1, turn.card
   end
 
   def test_turn_has_a_guess
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card_1)
+    turn = Turn.new("Juneau", @card_1)
 
     assert_equal "Juneau", turn.guess
   end
 
   def test_the_guess_is_correct
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card_1)
+    turn = Turn.new("Juneau", @card_1)
 
     assert_equal true, turn.correct?
   end
 
   def test_does_turn_give_feedback
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card_1)
+    turn = Turn.new("Juneau", @card_1)
 
     assert_equal "Correct!", turn.feedback
   end
