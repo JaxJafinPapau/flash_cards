e5ca7bace786f821ea32c00165c63db26455b84c
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-01-27 16:37:15 -0800

    Added number_correct method to round class

--- lib/round.rb
+++ lib/round.rb
@@ -4,13 +4,34 @@ require './lib/deck'
 require 'pry'
 
 class Round
+  attr_reader :deck,
+              :turns,
+              :turn,
+              :turn_number
 
   def initialize(deck)
     @deck = deck
+    @turns = []
+    @turn_number = 0
   end
-  #
-  # def take_turn
-  #   #creates new turn object with guess and card
-  # end
 
+  def current_card
+    @turns[@turn_number]
+  end
+
+  def take_turn(guess)
+    @turns << Turn.new(@guess, @card)
+    @turn_number += 1
+    return @turns
+  end
+
+  def number_correct
+    total_correct = 0
+    @turns.each do |turn|
+      if turn.guess == card.answers
+        total_correct += 1
+      end
+    end
+    return total_correct
+  end
 end
